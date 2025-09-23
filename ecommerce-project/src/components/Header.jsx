6e9d1497c9d13c5332233fbf4d23d80b9b359dca
Divyanshu <dnd.mein@gmail.com>
2025-09-23 23:30:17 +0530

    lift card up and share it between pages

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -5,11 +5,11 @@ import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import { NavLink } from 'react-router'
-const Header = ({cart}) => {
-    let totalQuantity=0;
-    cart.forEach((cartItem)=>{
-        totalQuantity+=cartItem.quantity
-    })
+const Header = ({ cart = [] }) => {
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
   return (
              <div className="header">
             <div className="left-section">
