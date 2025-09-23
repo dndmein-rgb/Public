eb88c9f9185769935fd30e1a1cd0de39dfc97265
Divyanshu <dnd.mein@gmail.com>
2025-09-23 21:09:48 +0530

    Fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import './HomePage.css'
 import {products} from '../../starting-code/data/products'
 import Header from '../components/Header'
 const HomePage = () => {
+    fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+        return response.json()  
+    }).then((data)=>{
+            console.log(data)
+        })
   return (
     <>
            <title>Ecommerce Project</title> 
