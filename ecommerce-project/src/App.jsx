52a3a6acef88e54f32d62fade2f76ef8b39b78a1
Divyanshu <dnd.mein@gmail.com>
2025-09-24 16:59:43 +0530

    Generat Html for orderspage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart}/>}/>
       <Route path="checkout" element={<CheckoutPage cart={cart}/>}/>
-       <Route path="orders" element={<OrdersPage/>}/>
+       <Route path="orders" cart={cart} element={<OrdersPage/>}/>
        <Route path='tracking' element={<TrackingPage/>}/>
        <Route path='*' element={<Error/>}/>
     </Routes>
