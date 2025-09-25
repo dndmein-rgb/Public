88ec303925d80235d9a0ac2057ebfb319624c4ad
Divyanshu <dnd.mein@gmail.com>
2025-09-25 13:45:38 +0530

    addded images to icon and images

--- ecommerce-project/src/pages/home/ProductsGrid.jsx
+++ ecommerce-project/src/pages/home/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import { formatMoney } from '../../utils/money'
 
 const ProductsGrid = ({products}) => {
@@ -50,7 +51,15 @@ const ProductsGrid = ({products}) => {
                         Added
                     </div>
     
-                    <button className="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary"
+                        onClick={()=>{
+                            axios.post('/api/cart-items' ,{
+                                productId:product.id,
+                                quantity:1
+
+                            });
+                        }}
+                        >
                         Add to Cart
                     </button>
                     </div>
