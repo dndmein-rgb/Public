135dfc8653281d98113496df7744a76ffedc0934
Divyanshu <dnd.mein@gmail.com>
2025-09-26 16:57:00 +0530

    Use before Each to remove duplications

--- ecommerce-project/src/pages/home/product.test.jsx
+++ ecommerce-project/src/pages/home/product.test.jsx
@@ -1,4 +1,4 @@
-import {it,expect,describe,vi} from 'vitest'
+import {it,expect,describe,vi, beforeEach} from 'vitest'
 import { render ,screen} from '@testing-library/react';
 import Product from './Product';
 import userEvent from '@testing-library/user-event';
@@ -7,8 +7,11 @@ import axios from 'axios';
 vi.mock('axios');
 
 describe('Product Component',()=>{
-  it('displays te product correctly',()=>{
-    const product= {
+    let product ;
+    let loadCart;
+
+    beforeEach(()=>{
+        product= {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -18,8 +21,12 @@ describe('Product Component',()=>{
       },
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"]
-    };
-    const loadCart=vi.fn();
+    }
+    loadCart=vi.fn();
+    });
+
+
+  it('displays te product correctly',()=>{
     render(<Product product={product} loadCart={loadCart}/>)
 
     expect(
@@ -44,18 +51,7 @@ describe('Product Component',()=>{
   });
 
   it('adds a product to the cart',async()=>{
-    const product= {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"]
-    };
-    const loadCart=vi.fn();
+    
     render(<Product product={product} loadCart={loadCart}/>)
     const user=userEvent.setup();
     const addToCartButton= screen.getByTestId('add-to-cart-button');
